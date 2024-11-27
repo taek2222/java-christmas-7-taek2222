4c3548b8c38eabf652563c806e0cd3c5ef4fa80d
taek2222 <oot309@daum.net>
2024-11-27 16:25:29 +0900

    feat(Order): 최소 갯수 이상 미달성시 예외 처리

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,12 +1,23 @@
 package christmas.domain;
 
+import static christmas.constant.ErrorMessage.INVALID_ORDER;
+
 public class Order {
+
+    private static final int MINIMUM_QUANTITY = 1;
 
     private Menu menu;
     private int quantity;
 
     public Order(Menu menu, int quantity) {
+        validateQuantity(quantity);
         this.menu = menu;
         this.quantity = quantity;
+    }
+
+    private void validateQuantity(int quantity) {
+        if (quantity < MINIMUM_QUANTITY) {
+            throw new IllegalArgumentException(INVALID_ORDER.get());
+        }
     }
 }
