213a19d1482ec80ff1a6a1e6be23e2805df534c3
taek2222 <oot309@daum.net>
2024-11-27 21:39:16 +0900

    feat(OutputView): 이벤트 혜택 메시지 출력 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,10 +1,15 @@
 package christmas.view;
 
+import static christmas.constant.MessageConstant.EVENT_NOTICE;
 import static christmas.constant.MessageConstant.WELCOME;
 
 public class OutputView {
 
     public void printWelcomeMessage() {
         System.out.println(WELCOME.get());
+    }
+
+    public void printEventNoticeMessage(String date) {
+        System.out.printf(EVENT_NOTICE.get(date));
     }
 }
