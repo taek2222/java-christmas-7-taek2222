e82041d6bd822614e70efcaf139f0ce074a5f774
taek2222 <oot309@daum.net>
2024-11-27 13:13:46 +0900

    feat: 주문 받는 View 기능 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,17 +1,22 @@
 package christmas.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static christmas.constant.MessageConstant.ORDER;
 import static christmas.constant.MessageConstant.VISIT_DATE;
 import static christmas.validation.CommonValidator.validateIsNumeric;
-
-import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
     public int readVisitDate() {
         System.out.println(VISIT_DATE.get());
-        String input = Console.readLine();
+        String input = readLine();
 
         validateIsNumeric(input);
         return Integer.parseInt(input);
+    }
+
+    public String readOrder() {
+        System.out.println(ORDER.get());
+        return readLine();
     }
 }
