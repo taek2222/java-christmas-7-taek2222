82c157b3afbaa8eb11fceda0c2f07933cafacbe7
taek2222 <oot309@daum.net>
2024-11-28 13:14:50 +0900

    fix(Menu): 금액 설정 이상 수정

--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -10,19 +10,19 @@ import java.util.Arrays;
 
 public enum Menu {
     MUSHROOM_SOUP("양송이수프", 6_000, APPETIZER),
-    TAPAS("타파스", 6_000, APPETIZER),
-    CAESAR_SALAD("시저샐러드", 6_000, APPETIZER),
+    TAPAS("타파스", 5_500, APPETIZER),
+    CAESAR_SALAD("시저샐러드", 8_000, APPETIZER),
 
-    T_BONE_STEAK("티본스테이크", 6_000, MAIN),
-    BARBECUE_RIBS("바비큐립", 6_000, MAIN),
-    SEAFOOD_PASTA("해산물파스타", 6_000, MAIN),
+    T_BONE_STEAK("티본스테이크", 55_000, MAIN),
+    BARBECUE_RIBS("바비큐립", 54_000, MAIN),
+    SEAFOOD_PASTA("해산물파스타", 35_000, MAIN),
 
-    CHOCOLATE_CAKE("초코케이크", 6_000, DESSERT),
-    ICE_CREAM("아이스크림", 6_000, DESSERT),
+    CHOCOLATE_CAKE("초코케이크", 15_000, DESSERT),
+    ICE_CREAM("아이스크림", 5_000, DESSERT),
 
-    ZERO_COLA("제로콜라", 6_000, DRINK),
-    RED_WINE("레드와인", 6_000, DRINK),
-    CHAMPAGNE("샴페인", 6_000, DRINK);
+    ZERO_COLA("제로콜라", 3_000, DRINK),
+    RED_WINE("레드와인", 60_000, DRINK),
+    CHAMPAGNE("샴페인", 25_000, DRINK);
 
     private final String name;
     private final int price;
@@ -32,6 +32,10 @@ public enum Menu {
         this.name = name;
         this.price = price;
         this.menuType = menuType;
+    }
+
+    public boolean isMenuTypeDrink() {
+        return this.menuType == DRINK;
     }
 
     public static Menu findByName(String name) {
