8b679d45e1ae02ab985476537296d7f6952a6c16
taek2222 <oot309@daum.net>
2024-11-28 15:04:37 +0900

    feat(Benefits): 주말, 평일 할인 이벤트 추가
    - 메뉴 카운트 갯수에 따라 할인 가격을 부여하도록 했습니다.

--- src/main/java/christmas/domain/Benefits.java
+++ src/main/java/christmas/domain/Benefits.java
@@ -1,22 +1,43 @@
 package christmas.domain;
 
+import static christmas.domain.MenuType.DESSERT;
+import static christmas.domain.MenuType.MAIN;
+
 public class Benefits {
 
     private static final int D_DAY_BASE = 1000;
     private static final int D_DAY_DAILY = 100;
 
     private final int DDay;
+    private final int weekdays;
+    private final int weekend;
 
-    public Benefits(OrderDate orderDate) {
-        this.DDay = calculateDDay(orderDate);
+    public Benefits(OrderDate orderDate, Orders orders) {
+        this.DDay = calculateDDayBenefit(orderDate);
+        this.weekdays = calculateWeekdaysBenefit(orderDate, orders);
+        this.weekend = calculateWeekendBenefit(orderDate, orders);
     }
 
-    private int calculateDDay(OrderDate orderDate) {
+    private int calculateDDayBenefit(OrderDate orderDate) {
         Integer day = orderDate.calculateDDay();
         if (day == null) {
             return 0;
         }
 
-        return D_DAY_BASE + (D_DAY_DAILY * (day - 1));
+        return D_DAY_BASE + (D_DAY_DAILY * day);
+    }
+
+    private int calculateWeekdaysBenefit(OrderDate orderDate, Orders orders) {
+        if (orderDate.isWeekend())
+            return 0;
+
+        return orders.countMenuType(DESSERT) * 2_023;
+    }
+
+    private int calculateWeekendBenefit(OrderDate orderDate, Orders orders) {
+        if (!orderDate.isWeekend())
+            return 0;
+
+        return orders.countMenuType(MAIN) * 2_023;
     }
 }
