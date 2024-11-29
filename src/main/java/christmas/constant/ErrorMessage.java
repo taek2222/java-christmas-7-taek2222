4014c3145abf11b1577854431ac169b618415b8e
taek2222 <oot309@daum.net>
2024-11-29 12:29:25 +0900

    fix(ErrorMessage): 잘못된 에러 메시지 출력 수정

--- src/main/java/christmas/constant/ErrorMessage.java
+++ src/main/java/christmas/constant/ErrorMessage.java
@@ -2,8 +2,8 @@ package christmas.constant;
 
 public enum ErrorMessage {
     INVALID_NUMERIC("숫자만 입력 가능합니다. 잘못된 입력 : (%s) "),
-    INVALID_DATE("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
-    INVALID_ORDER("유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    INVALID_DATE("유효하지 않은 날짜입니다."),
+    INVALID_ORDER("유효하지 않은 주문입니다."),
     INVALID_ONLY_DRINK("음료만 주문할 수 없습니다."),
     INVALID_ORDER_QUANTITY("총 %d개 이상의 주문은 할 수 없습니다.")
     ;
